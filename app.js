7ed9e4ff216511a48d3e3d57dd4e32fe15fa07e0
Caleb Xu <calxu@iu.edu>
2024-04-25 11:31:35 -0400

    adding express.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.listen(POST, (error) =>{
+    if(!error){
+        console.log("Server is successfully running, and App is listening on port " + PORT)
+    }
+    else{
+        console.log("Error occured, server can't start", error);
+    }
+}
+);
