2c73b51a89d6d160ca84b7199a24756ba3aab90c
Caleb Xu <calxu@iu.edu>
2024-05-26 17:47:09 -0400

    testing website on silo server

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 //Modules
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
+
+//Connection to db
+const connection = mysql.createConnection({
+    host: 'db.luddy.indiana.edu',
+    user: 'i308s23_calxu',
+    password: 'my+sql=i308s23_calxu',
+    database: 'i308s23_calxu'
+});
+
+//Query
+connection.execute(
+    'SELECT * FROM Recipes',
+    function (err, results, fields) {
+        console.log(results);
+        console.log(fields);
+        console.log(err);
+    }
+)
 
 const app = express();
 const PORT = 3000;
-
-//Pathing
-let staticPath = path.join(__dirname, 'public');
 
 //Error catching
 app.listen(PORT, (error) =>{
@@ -20,7 +36,7 @@ app.listen(PORT, (error) =>{
 );
 
 //Setting up statics
-app.use(express.static(staticPath));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Setting up EJS (View Engine)
 app.set('view engine', 'ejs');
