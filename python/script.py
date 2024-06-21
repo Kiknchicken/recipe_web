8b419df6a8a5a73def42aefcd3e845bef009ee41
Caleb Xu <calxu@iu.edu>
2024-06-20 20:55:03 -0400

    bug fixing

--- python/script.py
+++ python/script.py
@@ -1,10 +1,14 @@
 from PIL import Image
+from io import BytesIO
 
 import pytesseract
 import sys
 import json
+import requests
 
-json_string = pytesseract.image_to_string(Image.open(sys.argv[1]))
+response = requests.get(sys.argv[1])
+
+json_string = pytesseract.image_to_string(Image.open(BytesIO(response.content)))
 
 json_data = {
     "image_data": json_string
