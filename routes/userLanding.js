2be39e611f1cca790c0370fca4e25e579be35187
Caleb Xu <calxu@iu.edu>
2024-07-10 14:56:32 -0400

    bug fixing

--- routes/userLanding.js
+++ routes/userLanding.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import { jwtDecode } from "jwt-decode";
+import { OAuth2Client } from 'google-auth-library';
 import util from 'util';
 
 const router = express.Router();
@@ -13,12 +14,21 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res, credential) => {
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: "630460929969-47is0aidbr8r9jitmhd12v3m6000875b.apps.googleusercontent.com",
+        });
+
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+    }
     console.log("userlanding");
 
     //Handling JWT token response
-    let token = credential;
-    // let deocdedToken = jwtDecode(token);
+    const client  = new OAuth2Client();
 
+    verify().catch(console.error);
     console.log(typeof token);
     res.render("userLanding");
 });
