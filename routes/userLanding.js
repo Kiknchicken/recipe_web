36017d8a4d1e7fe10623794f9b0ca70d0d1f5feb
Caleb Xu <calxu@iu.edu>
2024-07-15 14:50:22 -0400

    Added userLanding page + pages that it links to

--- routes/userLanding.js
+++ routes/userLanding.js
@@ -4,6 +4,9 @@ const express = require('express');
 //Defining routing object
 const router = express.Router();
 
+//Setting up statics
+app.use(express.static(path.join('public')));
+
 router.get("/", (req, res) => {
     console.log("userlanding");
     res.render("userLanding");
