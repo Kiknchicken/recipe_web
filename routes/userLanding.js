a413566b78cb8e5091f1422c03c9317d0346227f
Caleb Xu <calxu@iu.edu>
2024-07-16 11:43:12 -0400

    bug fixing

--- routes/userLanding.js
+++ routes/userLanding.js
@@ -4,7 +4,7 @@ const path = require("path");
 const router = express.Router();
 
 //Setting up statics
-router.use('/public', express.static(path.join(__dirname, 'public')));
+router.use(express.static(path.join(__dirname, 'public')));
 
 router.get("/", (req, res) => {
     console.log("userlanding");
