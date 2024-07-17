a2991711217c889bd6347b5e96a6840747b71086
Caleb Xu <calxu@iu.edu>
2024-07-17 19:18:33 -0400

    testing ejs for loop

--- routes/discover.js
+++ routes/discover.js
@@ -6,9 +6,13 @@ const router = express.Router();
 //Setting up statics
 router.use(express.static(path.join(__dirname, '..', 'public')));
 
+//Stuff
+const cards_num = 5;
+
+//Routes
 router.get("/", (req, res) => {
     console.log("discover");
-    res.render("discover", { name: 'Guest' });
+    res.render("discover", { name: 'Guest', cards_num: cards_num});
     console.log(path.join(__dirname, '..', 'public'));
 });
 
