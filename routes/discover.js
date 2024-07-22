dd9a815e46aacbb9fcc3d2f814b00cda692e8fd5
Caleb Xu <calxu@iu.edu>
2024-07-21 20:07:38 -0400

    bug fixing

--- routes/discover.js
+++ routes/discover.js
@@ -50,7 +50,7 @@ client.query('SELECT * FROM recipes', (err, res) => {
 //Routes
 router.get("/", (req, res) => {
     console.log("discover");
-    res.render("discover", { name: 'Guest', num_cards: num_cards, fried_rice: fried_rice, id: id});
+    res.render("discover", { name: 'Guest', num_cards: num_cards, query_result: data, id: id});
     console.log(path.join(__dirname, '..', 'public'));
     console.log(query_result);
 });
