76a90fb41e3becba4ebc0be7d854c1b2acb94721
Caleb Xu <calxu@iu.edu>
2024-07-24 16:51:34 -0400

    Implementing recipe ejs

--- routes/recipe.js
+++ routes/recipe.js
@@ -2,9 +2,40 @@
 const express = require('express');
 const path = require("path");
 const router = express.Router();
+const { Client } = require('pg');
 
 //Setting up statics
 router.use(express.static(path.join(__dirname, '..', 'public')));
+
+//Functions
+async function getRecipe(id) {
+    //Connection to db
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+        rejectUnauthorized: false
+        }
+    });
+
+    client.connect();
+
+    const query = {
+        text: "SELECT * FROM recipes WHERE id=$1::text",
+        values: [id],
+        rowMode: 'array'
+    };
+
+    //Query
+    var data = [];
+
+    var result = await client.query(query);
+
+    for (let i = 0; i < result.rows.length; i++) {
+        data.push(result.rows[i]);
+    }
+    
+    return data;
+}
 
 router.get("/", (req, res) => {
     console.log("recipes");
@@ -12,9 +43,14 @@ router.get("/", (req, res) => {
     console.log(path.join(__dirname, '..', 'public'));
 });
 
-router.post("/", (req, res) => {
-    console.log(req.body.id);
-    res.render("recipe");
+router.post("/", async (req, res) => {
+    var id = req.body.id.toString();
+    console.log(id);
+
+    //Grab recipe from db
+    var data = await getRecipe(id);
+
+    res.render("recipe", { id: id, data: data });
 });
 
 module.exports = router;
