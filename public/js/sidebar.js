e5bfefcac1f4670501537b8f2577b78738f00c82
Caleb Xu <calxu@iu.edu>
2024-07-29 13:32:18 -0400

    created filters that work with db

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,29 +1,58 @@
 /* sideBar */
-function card(cuisine_type) {
-    /* Init vars */
-    const tags  = {
-        "cuisine": []
-    };
+function card(header, header_data) {
 
+    /* grab headers from dict */
     let headers = Object.keys(tags);
     let filter_string = "";
 
     /* Add cuisine value to dictionary */
-    tags["cuisine"].push(cuisine_type);
+    if (tags[header].includes(header_data)) {
+        console.log("YARR");
+        tags[header].pop();
+    } else {
+        console.log("NAURRR");
+        tags[header].push(header_data);
+    }
 
     /* display recipes that are in dictionary */
     for (let i = 0; i < headers.length; i++) {
-        filter_string += '[data-' + headers[i] + ']:not([data-' + headers[i] + '="' + tags[headers[i]] + '"])';
+        if (tags[headers[i]].length != 0) {
+            filter_string += '[data-' + headers[i] + ']:not';
+            for (let j = 0; j < tags[headers[i]].length; j++) {
+                filter_string += '([data-' + headers[i] + '="' + tags[headers[j]] + '"]):or';
+            }
+        } 
     }
 
-    let cards = document.querySelectorAll(filter_string);
+    filter_string = filter_string.substring(0, filter_string.length - 3);
 
-    for (let i = 0; i < cards.length; i++) {
-        if (cards[i].getAttribute("class") == "minimize") {
-            cards[i].setAttribute("class", "card")
-        } else {
-            cards[i].setAttribute("class", "minimize");
+    // filter_string += '[data-' + headers[i] + ']:not([data-' + headers[i] + '="' + tags[headers[i]] + '"])';
+
+    if (filter_string.length == 0) {
+        let cards = document.querySelectorAll(".minimize");
+        for (let i = 0; i < cards.length; i++) {
+            
+            cards[i].setAttribute("class", "card");
         }
+        
+    } else {
+        let cards = document.querySelectorAll(filter_string);
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].setAttribute("class", "minimize");
+        }  
     }
 }
 
+/* Init dict */
+const tags = {};
+
+/* Grab info from div */
+let filterHeaders = document.getElementById("filterData").getAttribute("data-filterHeaders").split(",");
+
+/* Parse through list and add headers to dict*/
+for (let i = 0; i < filterHeaders.length; i++) {
+    tags[filterHeaders[i]] = [];
+}
+
+console.log(filterHeaders);
+console.log(tags);
